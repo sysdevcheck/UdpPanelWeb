e60ec2afab97db22de68eaee8a4fa158e9333516
Lester Meléndez <sysdevcheck@gmail.com>
2025-10-21 22:38:01 +0000

    .

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -15,6 +16,28 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_OWNER_USERNAME: process.env.OWNER_USERNAME,
     NEXT_PUBLIC_OWNER_PASSWORD: process.env.OWNER_PASSWORD,
+  },
+  webpack: (config, { isServer }) => {
+    // Exclude ssh2 from being bundled in the client-side build
+    if (!isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'ssh2': 'ssh2',
+      });
+    }
+    // For server-side, you might need to mark it as external 
+    // if the environment doesn't support it, but for App Hosting
+    // we just want to get the build to pass. We can mark it as external
+    // and expect it to fail at runtime.
+    else {
+        config.externals = config.externals || [];
+        config.externals.push({
+            'ssh2': 'ssh2',
+        });
+    }
+
+
+    return config;
   },
 };
 
