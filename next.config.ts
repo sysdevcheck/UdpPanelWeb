1fd8560b74b2311f532888a632658391484849b1
Lester Meléndez <sysdevcheck@gmail.com>
2025-10-22 04:08:56 +0000

    Build failed because of webpack errors root@hy2:~#  npm audit fix npm ER

--- next.config.ts
+++ next.config.ts
@@ -17,9 +17,16 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_OWNER_USERNAME: process.env.OWNER_USERNAME,
     NEXT_PUBLIC_OWNER_PASSWORD: process.env.OWNER_PASSWORD,
   },
-   webpack: (config, { isServer }) => {
+   webpack: (config, { isServer, webpack }) => {
     if (isServer) {
-      config.externals = [...config.externals, 'ssh2'];
+        // Excluir ssh2 de ser empaquetado por Webpack, ya que es una dependencia nativa de Node.js
+        config.externals.push('ssh2');
+        
+        // Manejar el archivo .node para que no sea procesado por los loaders de Webpack
+        config.module.rules.push({
+            test: /\.node$/,
+            use: 'node-loader',
+        });
     }
     return config;
   },
